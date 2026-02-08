2bd340d1808ed37f5cdca3730d302897628b8e2a
mohamedelbagoury11 <231975199+MohamedElbagoury11@users.noreply.github.com>
2026-02-08 22:07:01 +0200

    'new_build'

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
 });
+
